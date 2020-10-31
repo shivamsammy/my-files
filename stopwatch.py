9f997d81ba8eb664df324c81a8335fecf23b9c05
shivamsammy <66337279+shivamsammy@users.noreply.github.com>
2020-10-31 08:58:38 +0530

    Delete stopwatch.py

--- stopwatch.py
+++
@@ -1,7 +0,0 @@
-import time
-print(" -- Your stopwatch is ready --")
-seconds=int(input(" enter the total number of seconds :"))
-for second in range(seconds):
-    print(str(seconds-second)+ " seconds remaining")
-    time.sleep(1)
-print(" TIME IS UP!!")
